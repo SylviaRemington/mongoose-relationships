5498d550a112f6635d966d1743813a7496c6b974
SylviaRemington <sylviaremington@gmail.com>
2025-07-04 14:58:47 -0700

    adding query functions and run query code underneath connect lines 28-lines49

--- queries.js
+++ queries.js
@@ -24,3 +24,26 @@ const connect = async () => {
 };
 
 connect();
+
+/*----------------------------- Query Functions -----------------------------*/
+
+const createTodo = async () => {
+  const todoData = {
+    text: 'learn React',
+    isComplete: false,
+  };
+  const todo = await Todo.create(todoData);
+  console.log('New todo:', todo);
+};
+
+const findTodos = async () => {
+  const todos = await Todo.find({});
+  console.log('All todos:', todos);
+};
+
+/*------------------------------- Run Queries -------------------------------*/
+
+const runQueries = async () => {
+  console.log('Queries running.');
+  await createTodo();
+};

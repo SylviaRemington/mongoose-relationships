5e8bd13976ed5c4021065a37504100a5c5e2b658
SylviaRemington <sylviaremington@gmail.com>
2025-07-04 14:54:47 -0700

    adding a try catch to the connecting to db section lines 11-24

--- queries.js
+++ queries.js
@@ -1,14 +1,16 @@
 // Mongoose Relationships Lecture with Tristan Hall
 // Not using express in this - just doing this as pure mongoose queries / we can always put it into an express app later 
 
+
 const dotenv = require("dotenv");
 dotenv.config();
 const mongoose = require("mongoose");
-const Todo = require('./models/todo.js');
+const Todo = require('./models/todo.js'); //getting our Todo model
 
+
+//Connects to the DB
 const connect = async () => {
     try {
-
         await mongoose.connect(process.env.MONGODB_URI);
         console.log('Connected to MongoDB');
         await runQueries();
@@ -16,7 +18,9 @@ const connect = async () => {
         await mongoose.disconnect();
         console.log('Disconnected from MongoDB');
         process.exit();
-    } catch (error) //! start from here with code from 38 min in
+    } catch (error) {
+        console.log(error);
+    }
 };
 
 connect();
